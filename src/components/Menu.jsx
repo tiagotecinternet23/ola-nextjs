4766980d08b6d1b4ced2a52f98d4b12f19064cfe
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-16 11:47:23 -0300

    Exemplo geral de pages

---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Equipe</a>
+        </li>
+        <li>
+          <a href="">Serviços</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Olá Next.js!</h1>
+      <Menu />
+      <main>
+        <h2>Bem-vindo ao Next.js</h2>
+        <h3>Página Inicial</h3>
+      </main>
+    </>
+  );
+}
